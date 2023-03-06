d8c15231847948e7db4b7aa955119d6f4f409e3e
Leoncio <leonciopimentelperez@gmail.com>
2023-03-05 20:34:26 -0600

    correcion

--- Temperatura.py
+++ Temperatura.py
@@ -6,7 +6,7 @@ class temperatura:
     def temperatura(): 
         while True:
             sensor = Adafruit_DHT.DHT11 #Cambia por DHT22 y si usas dicho sensor
-            pin = 4 #Pin en la raspberry donde conectamos el sensor
+            pin = 11 #Pin en la raspberry donde conectamos el sensor
             humedad, temperatura = Adafruit_DHT.read_retry(sensor, pin)
             
             print ('Humedad: ' , humedad)
